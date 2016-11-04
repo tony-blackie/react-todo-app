8922545dc4595a695fc05f34d5fb915256b3696f
Anton Spaskiy <anton.spaskiy@dataart.com>
2016-11-04 14:45:48 +0100

    Fix error that crashed app because array of todos was empty

--- src/containers/App.js
+++ src/containers/App.js
@@ -73,28 +73,55 @@ let nextTodoId = 0;
 
 class TodoApp extends Component {
     render() {
-        return (
-            <div>
-                <button onClick={() => {
-                    store.dispatch({
-                        type: 'ADD_TODO',
-                        text: 'Test',
-                        id: nextTodoId++
+        if (this.props.todos) {
+            return (
+                <div>
+                    <input ref={node => {
+                        this.input = node;
+                    }}
+                    />
+                    <button onClick={() => {
+                        store.dispatch({
+                            type: 'ADD_TODO',
+                            text: this.input.value,
+                            id: nextTodoId++
+                        });
+                        this.input.value = '';
+                    }}>
+                        Add Todo
+                    </button>
+                    <ul>
+                        {
+                            this.props.todos.map(todo =>
+                                <li key={todo.id}>
+                                    {todo.text}
+                                </li>
+                            )}
+                    </ul>
 
-                })
-                }}>
-                    Add Todo
-                </button>
-                <ul>
-                    {this.props.todos.map(todo =>
-                        <li key={todo.id}>
-                            {todo.text}
-                        </li>
-                    )}
-                </ul>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <input ref={node => {
+                        this.input = node;
+                    }}
+                    />
+                    <button onClick={() => {
+                        store.dispatch({
+                            type: 'ADD_TODO',
+                            text: this.input.value,
+                            id: nextTodoId++
+                        });
+                        this.input.value = '';
+                    }}>
+                        Add Todo
+                    </button>
+                </div>
+            );
+        }
 
-            </div>
-        );
     }
 }
 
